8d1ff0da68d5e37673ea0835adb9017099a28a6a
Tanmay Chaudhari <hctanmay@gmail.com>
2023-12-28 00:08:42 -0500

    :rocket: Update env variable setup

--- app/db.py
+++ app/db.py
@@ -1,10 +1,10 @@
 import os
 from pymongo import MongoClient
-from dotenv import load_dotenv
-from pathlib import Path
+# from dotenv import load_dotenv
+# from pathlib import Path
 
-dotenv_path = Path('../.env')
-load_dotenv(dotenv_path=dotenv_path)
+# dotenv_path = Path('../.env')
+# load_dotenv(dotenv_path=dotenv_path)
 
 uri = "mongodb+srv://tanmayc:" + os.getenv('CONNECTION_PASS') + "@cluster0.ls88hji.mongodb.net/?retryWrites=true&w=majority"
 
